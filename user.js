2d1ce9833c3580d737fdc60f10405e6e177c32ef
bilibili-開飛機のzm <154767397+Bilibilizm@users.noreply.github.com>
2025-01-04 19:23:05 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         GeoFS Flight Inspection Checklist
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  Please use L to open or close the menu
+// @version      1.2
+// @description  Thanks for useing!Good Flight!
 // @author       zm
 // @match        https://www.geo-fs.com/geofs.php?v=3.9
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
